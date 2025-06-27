036a4529abde80bab0d72692e312404e8117724b
Muhammad Robitulloh <67168926+muhammad-robitulloh@users.noreply.github.com>
2025-06-27 23:47:29 +0700

    Create __init__.py

---
+++ cognitive_shell/__init__.py
@@ -0,0 +1,16 @@
+"""
+MyProject - Modul Python MVP dari Darksideeffect 
+------------------------------------------------
+Desain modular untuk automasi, AI, dan integrasi edge-to-cloud.
+
+© 2025 Darksideeffect. All rights reserved.
+"""
+
+__version__ = "0.3.0"
+__author__ = "Muhammad Robitulloh, Founder & CEO Darksideeffect developer of NeuroNet AI"
+__license__ = "Proprietary"
+
+# Public API
+from .core import *
+from .utils import *
+from .config import settings
